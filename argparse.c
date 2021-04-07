ef35c26308a8ef961a5c5bb28222c492bd2c60c7
r2kapig <99670839+r2kapig@users.noreply.github.com>
2021-04-07 09:36:14 +0000

    bonding: Remove debug_fs files when module init fails

--- argparse.c
+++ argparse.c
@@ -2,4 +2,4 @@
  * Copyright (C) 2012-2015 Yecheng Fu <cofyc.jackson at gmail dot com>
  * All rights reserved.
  *
- * Use of this source code is governed by a MIT-styl
+ * Use of this source code is governed by a MIT-style licen
