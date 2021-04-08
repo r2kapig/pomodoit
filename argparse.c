4cc721ebe121f1b52c11df3825f7fcae25a4268b
r2kapig <99670839+r2kapig@users.noreply.github.com>
2021-04-08 06:45:56 +0000

    rand

--- argparse.c
+++ argparse.c
@@ -2,4 +2,5 @@
  * Copyright (C) 2012-2015 Yecheng Fu <cofyc.jackson at gmail dot com>
  * All rights reserved.
  *
- * Use of this source code is governed by a MIT-style license that can be f
+ * Use of this source code is governed by a MIT-style license that can be found
+ * in 
