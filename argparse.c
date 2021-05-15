8606cf275e5703245df52f9307b17039422ddb04
r2kapig <99670839+r2kapig@users.noreply.github.com>
2021-05-15 16:38:33 +0000

    Reorder functions

--- argparse.c
+++ argparse.c
@@ -34,4 +34,5 @@ prefix_cmp(const char *str, const char *prefix)
 }
 
 static void
-argparse_error(struct argparse *self, const struct argparse_option *
+argparse_error(struct argparse *self, const struct argparse_option *opt,
+   
