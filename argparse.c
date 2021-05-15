fabb0110e1828f7960da7ea94fef3aadc6960921
r2kapig <99670839+r2kapig@users.noreply.github.com>
2021-05-15 08:37:15 +0000

    sched: add wait_for_completion_io[_timeout]

--- argparse.c
+++ argparse.c
@@ -34,4 +34,4 @@ prefix_cmp(const char *str, const char *prefix)
 }
 
 static void
-argparse_error(struct argparse *self, const struct argparse_op
+argparse_error(struct argparse *self, const struct argparse_option *
