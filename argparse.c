09b2fa44af820a65eff916390c51ceb0dce60952
r2kapig <99670839+r2kapig@users.noreply.github.com>
2021-06-15 08:00:07 +0000

    Prerequisites

--- argparse.c
+++ argparse.c
@@ -55,4 +55,5 @@ argparse_getvalue(struct argparse *self, const struct argparse_option *opt,
         goto skipped;
     switch (opt->type) {
     case ARGPARSE_OPT_BOOLEAN:
-        if (flags & OPT_UNSE
+        if (flags & OPT_UNSET) {
+          
