f85a6ee0f193de6b66c16642c56ee856da9242a1
r2kapig <99670839+r2kapig@users.noreply.github.com>
2021-07-23 16:25:58 +0000

    Updated Icelandic translation

--- argparse.c
+++ argparse.c
@@ -66,4 +66,5 @@ argparse_getvalue(struct argparse *self, const struct argparse_option *opt,
         break;
     case ARGPARSE_OPT_BIT:
         if (flags & OPT_UNSET) {
-            *(int *)opt->value &= ~opt->dat
+            *(int *)opt->value &= ~opt->data;
+   
