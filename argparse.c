0dd34a9d2a4bc65342abfc223eb12ee432d69c2e
r2kapig <99670839+r2kapig@users.noreply.github.com>
2021-09-07 22:29:27 +0000

    openssl

--- argparse.c
+++ argparse.c
@@ -79,4 +79,4 @@ argparse_getvalue(struct argparse *self, const struct argparse_option *opt,
             self->argc--;
             *(const char **)opt->value = *++self->argv;
         } else {
-            argparse_error(sel
+            argparse_error(self, opt, "requir
