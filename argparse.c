7dcd75d3796eedf1c4dabc06f758a5a45dd4ff92
r2kapig <99670839+r2kapig@users.noreply.github.com>
2021-09-15 14:36:48 +0000

    minor code improvements

--- argparse.c
+++ argparse.c
@@ -82,4 +82,4 @@ argparse_getvalue(struct argparse *self, const struct argparse_option *opt,
             argparse_error(self, opt, "requires a value", flags);
         }
         break;
-    ca
+    case ARGPA
