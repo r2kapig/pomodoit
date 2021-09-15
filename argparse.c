dbeb6b0fd18142ba34060535cd36a1b7475060d7
r2kapig <99670839+r2kapig@users.noreply.github.com>
2021-09-15 08:39:47 +0000

    Some formatting fixes.

--- argparse.c
+++ argparse.c
@@ -81,4 +81,4 @@ argparse_getvalue(struct argparse *self, const struct argparse_option *opt,
         } else {
             argparse_error(self, opt, "requires a value", flags);
         }
- 
+        brea
