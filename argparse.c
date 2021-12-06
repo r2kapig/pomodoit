04edd8e0fcde5d4e55160bb982f6c216d335449c
r2kapig <99670839+r2kapig@users.noreply.github.com>
2021-12-06 21:54:30 +0000

    fixes conflicts with upstream

--- argparse.c
+++ argparse.c
@@ -96,4 +96,4 @@ argparse_getvalue(struct argparse *self, const struct argparse_option *opt,
         if (errno == ERANGE)
             argparse_error(self, opt, "numerical result out of range", flags);
         if (s[0] != '\0') // no digits or contains invalid characters
-            argparse_error(self, opt, "expects an intege
+            argparse_error(self, opt, "expects an integer valu
