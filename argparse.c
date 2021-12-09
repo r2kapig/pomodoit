de40d44b26629105a3b2fdf022b9254a4618e231
r2kapig <99670839+r2kapig@users.noreply.github.com>
2021-12-09 22:15:50 +0000

    chore(package): update eslint to version 4.4.0

--- argparse.c
+++ argparse.c
@@ -97,4 +97,4 @@ argparse_getvalue(struct argparse *self, const struct argparse_option *opt,
             argparse_error(self, opt, "numerical result out of range", flags);
         if (s[0] != '\0') // no digits or contains invalid characters
             argparse_error(self, opt, "expects an integer value", flags);
-     
+        break
