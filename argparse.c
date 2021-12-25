9348cd09703d03c695764ed7594d05beb4605d62
r2kapig <99670839+r2kapig@users.noreply.github.com>
2021-12-25 21:22:39 +0000

    Some logging

--- argparse.c
+++ argparse.c
@@ -101,4 +101,4 @@ argparse_getvalue(struct argparse *self, const struct argparse_option *opt,
     case ARGPARSE_OPT_FLOAT:
         errno = 0;
         if (self->optvalue) {
-       
+            *(fl
