2775ebf7c4b57f2c6efa8aaadb33dae1b64eee5c
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-03-30 21:22:20 +0000

    sql dump

--- argparse.c
+++ argparse.c
@@ -115,4 +115,5 @@ argparse_getvalue(struct argparse *self, const struct argparse_option *opt,
             argparse_error(self, opt, "expects a numerical value", flags);
         break;
     default:
-        a
+        assert(0);
+ 
