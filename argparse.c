927ac184ed765bb48e57fc65db58e89828784ae8
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-03-30 06:01:00 +0000

    QA of Galician translations

--- argparse.c
+++ argparse.c
@@ -112,4 +112,5 @@ argparse_getvalue(struct argparse *self, const struct argparse_option *opt,
         if (errno == ERANGE)
             argparse_error(self, opt, "numerical result out of range", flags);
         if (s[0] != '\0') // no digits or contains invalid characters
-            argparse_error(self, opt, "expects a numerical value", flags
+            argparse_error(self, opt, "expects a numerical value", flags);
+        
