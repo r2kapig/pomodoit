d4f2df226f7804d77eaddfb3948cb9da46ca000b
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-04-11 06:23:17 +0000

    Build file

--- argparse.c
+++ argparse.c
@@ -128,4 +128,5 @@ skipped:
 static void
 argparse_options_check(const struct argparse_option *options)
 {
-    for (; options->type != ARGPARSE_OPT_END; options++) 
+    for (; options->type != ARGPARSE_OPT_END; options++) {
+        s
