efde58f0111dc9b0a4c862eda55e7f29592f786a
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-04-13 13:08:17 +0000

    Lint errors

--- argparse.c
+++ argparse.c
@@ -131,4 +131,4 @@ argparse_options_check(const struct argparse_option *options)
     for (; options->type != ARGPARSE_OPT_END; options++) {
         switch (options->type) {
             case ARGPARSE_OPT_END:
-            ca
+            case A
