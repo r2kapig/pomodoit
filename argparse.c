42eaf9b81716cb29ac8b1c81ed2042d4fb179c29
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-05-13 18:23:51 +0000

    Update App.xaml.cs

--- argparse.c
+++ argparse.c
@@ -160,4 +160,4 @@ argparse_short_opt(struct argparse *self, const struct argparse_option *options)
 static int
 argparse_long_opt(struct argparse *self, const struct argparse_option *options)
 {
-    for (; options->type != ARGPARSE_OPT_E
+    for (; options->type != ARGPARSE_OPT_END; option
