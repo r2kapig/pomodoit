0644aec3cb4793318d86d87476971e6775cfe78b
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-05-18 20:10:00 +0000

    further tweaks

--- argparse.c
+++ argparse.c
@@ -168,4 +168,5 @@ argparse_long_opt(struct argparse *self, const struct argparse_option *options)
 
         rest = prefix_skip(self->argv[0] + 2, options->long_name);
         if (!rest) {
-            // negation disabl
+            // negation disabled?
+     
