c309b83ab8d43f6a215e636b0ce30272bda27e2c
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-05-18 09:29:45 +0000

    Version bump to 0.4.1

--- argparse.c
+++ argparse.c
@@ -166,4 +166,5 @@ argparse_long_opt(struct argparse *self, const struct argparse_option *options)
         if (!options->long_name)
             continue;
 
-        rest = prefix_skip(self->argv[0] + 2, options->long_nam
+        rest = prefix_skip(self->argv[0] + 2, options->long_name);
+        if (!r
