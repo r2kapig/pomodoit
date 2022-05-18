6ad8c7d272d8d81c336a9dd45117bdee4f5d06b5
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-05-18 18:13:57 +0000

    added a favicon

--- argparse.c
+++ argparse.c
@@ -168,4 +168,4 @@ argparse_long_opt(struct argparse *self, const struct argparse_option *options)
 
         rest = prefix_skip(self->argv[0] + 2, options->long_name);
         if (!rest) {
-            // ne
+            // negation di
