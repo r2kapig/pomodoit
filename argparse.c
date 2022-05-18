6ced25c73cdb1ae5e522ea07f413155fcc094255
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-05-18 12:30:17 +0000

    missing #include

--- argparse.c
+++ argparse.c
@@ -167,4 +167,5 @@ argparse_long_opt(struct argparse *self, const struct argparse_option *options)
             continue;
 
         rest = prefix_skip(self->argv[0] + 2, options->long_name);
-        if (!r
+        if (!rest) {
+    
