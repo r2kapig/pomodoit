e6881a881857be5fa0a22d6d0f72441789e43bfd
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-06-16 12:46:08 +0000

    Update composer lock.

--- argparse.c
+++ argparse.c
@@ -184,4 +184,4 @@ argparse_long_opt(struct argparse *self, const struct argparse_option *options)
             rest = prefix_skip(self->argv[0] + 2 + 3, options->long_name);
             if (!rest)
                 continue;
- 
+        
