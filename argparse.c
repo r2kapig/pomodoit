339e158c699e2a556e337b9647e2fcffc9723df4
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-06-28 13:06:58 +0000

    add documentation

--- argparse.c
+++ argparse.c
@@ -197,4 +197,4 @@ argparse_long_opt(struct argparse *self, const struct argparse_option *options)
 }
 
 int
-argparse_init(struct argparse *self, struct argparse_option
+argparse_init(struct argparse *self, struct argparse_option *opti
