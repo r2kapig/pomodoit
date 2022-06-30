7c119fd672c336eacf6116f548627608232a364a
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-06-30 16:35:19 +0000

    Internal Change.

--- argparse.c
+++ argparse.c
@@ -201,4 +201,4 @@ argparse_init(struct argparse *self, struct argparse_option *options,
               const char *const *usages, int flags)
 {
     memset(self, 0, sizeof(*self));
-    
+    self->opti
