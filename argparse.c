a9d5a036b8c13f95d6ae0fb3af72914403f316de
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-07-06 10:25:57 +0000

    done!

--- argparse.c
+++ argparse.c
@@ -204,4 +204,5 @@ argparse_init(struct argparse *self, struct argparse_option *options,
     self->options     = options;
     self->usages      = usages;
     self->flags       = flags;
-    self->description = NUL
+    self->description = NULL;
+  
