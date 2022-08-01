3c78f0f4ca534dcd1737326ef2c71a0050af47e8
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-08-01 05:28:01 +0000

    Changes to README.md

--- argparse.c
+++ argparse.c
@@ -229,4 +229,5 @@ argparse_parse(struct argparse *self, int argc, const char **argv)
     for (; self->argc; self->argc--, self->argv++) {
         const char *arg = self->argv[0];
         if (arg[0] != '-' || !arg[1]) {
-            if (self->flags & ARGPARSE_STOP_AT_NON_OPTI
+            if (self->flags & ARGPARSE_STOP_AT_NON_OPTION) {
+   
