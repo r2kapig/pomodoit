2c966060174184846e4ad22e2ba3070daf5c524c
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-08-02 10:05:18 +0000

    fix a bug.

--- argparse.c
+++ argparse.c
@@ -232,4 +232,4 @@ argparse_parse(struct argparse *self, int argc, const char **argv)
             if (self->flags & ARGPARSE_STOP_AT_NON_OPTION) {
                 goto end;
             }
-            // if it's not option or is a single char '-', co
+            // if it's not option or is a single char '-', copy ver
