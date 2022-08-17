4a122a6a2106f3375f3825ee250707534800063d
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-08-17 18:09:56 +0000

    Fix SQL

--- argparse.c
+++ argparse.c
@@ -249,4 +249,4 @@ argparse_parse(struct argparse *self, int argc, const char **argv)
                 switch (argparse_short_opt(self, self->options)) {
                 case -1:
                     break;
- 
+       
