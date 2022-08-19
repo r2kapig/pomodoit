1e2a0074c10852acbb7a315b9ea005735c32fc67
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-08-19 21:50:33 +0000

    Updated docs from build [ci skip]

--- argparse.c
+++ argparse.c
@@ -250,4 +250,5 @@ argparse_parse(struct argparse *self, int argc, const char **argv)
                 case -1:
                     break;
                 case -2:
-                    goto unkn
+                    goto unknown;
+ 
