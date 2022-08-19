ea117d896135f7d461eb22f65ef883c877466af9
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-08-19 22:49:21 +0000

    removed useless comment

--- argparse.c
+++ argparse.c
@@ -251,4 +251,4 @@ argparse_parse(struct argparse *self, int argc, const char **argv)
                     break;
                 case -2:
                     goto unknown;
-       
+                }
