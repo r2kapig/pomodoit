40bd65a9315e6c27e2affc61c09c01866edabec5
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-08-31 07:42:22 +0000

    Increasing test coverage

--- argparse.c
+++ argparse.c
@@ -264,4 +264,4 @@ argparse_parse(struct argparse *self, int argc, const char **argv)
         // long option
         switch (argparse_long_opt(self, self->options)) {
         case -1:
-           
+            break;
