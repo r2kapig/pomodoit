49d7c13de7bced79eedc1c9227a9e93ef8bb8572
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-09-06 16:42:59 +0000

    Added simple test

--- argparse.c
+++ argparse.c
@@ -273,4 +273,4 @@ argparse_parse(struct argparse *self, int argc, const char **argv)
 unknown:
         fprintf(stderr, "error: unknown option `%s`\n", self->argv[0]);
         argparse_usage(self);
-        if (!
+        if (!(self->
