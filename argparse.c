c38449817c0bb0028630384100a4efe9233e0b97
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-09-06 12:28:18 +0000

    Removed redundant test

--- argparse.c
+++ argparse.c
@@ -272,4 +272,5 @@ argparse_parse(struct argparse *self, int argc, const char **argv)
 
 unknown:
         fprintf(stderr, "error: unknown option `%s`\n", self->argv[0]);
-        argparse_usage(sel
+        argparse_usage(self);
+    
