46ee009c3ee951b9c4c4a3df90a596e730d76460
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-09-13 07:55:04 +0000

    Delete _config.yml~

--- argparse.c
+++ argparse.c
@@ -281,4 +281,4 @@ unknown:
 end:
     memmove(self->out + self->cpidx, self->argv,
             self->argc * sizeof(*self->out));
-    self->out[self->cpidx + self->argc] 
+    self->out[self->cpidx + self->argc] = NULL;
