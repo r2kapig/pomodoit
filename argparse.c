69184787978d09fc36fdbc210eae309710468afd
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-10-05 10:20:37 +0000

    remove icons

--- argparse.c
+++ argparse.c
@@ -314,4 +314,4 @@ argparse_usage(struct argparse *self)
         if ((options)->short_name) {
             len += 2;
         }
-        if ((options)->short_name &&
+        if ((options)->short_name && (options)->lon
