e2cbea519119771ac0f7bcc7feab95fffd6024ce
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-10-25 21:56:41 +0000

    \t* Makefile.am: Run "make dep-am". \t* Makefile.in: Regenerate. \t* po/POTFILES.in: Regenerate.

--- argparse.c
+++ argparse.c
@@ -336,4 +336,4 @@ argparse_usage(struct argparse *self)
     usage_opts_width += 4;      // 4 spaces prefix
 
     options = self->options;
-    for (; options->type != ARGPA
+    for (; options->type != ARGPARSE_OPT_END; 
