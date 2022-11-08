b110502ed710d31c70bb8d8594a8f35a2516bd20
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-11-08 13:42:21 +0000

    Clean up repo

--- argparse.c
+++ argparse.c
@@ -347,4 +347,4 @@ argparse_usage(struct argparse *self)
         }
         pos = fprintf(stdout, "    ");
         if (options->short_name) {
-            pos += fprintf(stdout, "-%c", options->shor
+            pos += fprintf(stdout, "-%c", options->short_name);
