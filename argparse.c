91c9fb3c34f18f348583872e2d49c3a8f817493a
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-11-21 15:55:56 +0000

    Javadoc cleanup.

--- argparse.c
+++ argparse.c
@@ -353,4 +353,4 @@ argparse_usage(struct argparse *self)
             pos += fprintf(stdout, ", ");
         }
         if (options->long_name) {
-            pos += fprintf(st
+            pos += fprintf(stdout, "-
