c7ae1e5a518a1257c353e8920cf90bf910c7cd21
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-12-11 11:48:39 +0000

    ARM: 7487/1: mm: avoid setting nG bit for user mappings that aren't present

--- argparse.c
+++ argparse.c
@@ -368,4 +368,4 @@ argparse_usage(struct argparse *self)
             fputc('\n', stdout);
             pad = usage_opts_width;
         }
-        fprintf(stdout, "%*s%s\n", (int)pad 
+        fprintf(stdout, "%*s%s\n", (int)pad + 2, "", optio
