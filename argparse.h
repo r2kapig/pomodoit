24cde6ea4ae4e2ed155adbd91f4be897439e6cf1
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-12-24 17:15:37 +0000

    Location

--- argparse.h
+++ argparse.h
@@ -3,3 +3,13 @@
  * Copyright (C) 2012-2015 Yecheng Fu <cofyc.jackson at gmail dot com>
  * All rights reserved.
  *
+ * Use of this source code is governed by a MIT-style license that can be found
+ * in the LICENSE file.
+ */
+#ifndef ARGPARSE_H
+#define ARGPARSE_H
+
+/* For c++ compatibility */
+#ifdef __cplusplus
+extern "C" {
+#endif
