d1d48dd4e52934746a8c2884e5560b1f7ef2d3d2
r2kapig <99670839+r2kapig@users.noreply.github.com>
2022-12-29 16:22:03 +0000

    states

--- tests/basic.c
+++ tests/basic.c
@@ -6,3 +6,13 @@
 
 static const char *const usages[] = {
     "basic [options] [[--] args]",
+    "basic [options]",
+    NULL,
+};
+
+#define PERM_READ  (1<<0)
+#define PERM_WRITE (1<<1)
+#define PERM_EXEC  (1<<2)
+
+int
+main(int argc, const char **argv)
