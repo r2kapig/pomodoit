3fcd3ce6572c761d9ff2a438e2f23554977a41ec
r2kapig <99670839+r2kapig@users.noreply.github.com>
2023-01-02 20:37:23 +0000

    Changed package name.

--- tests/subcommands.c
+++ tests/subcommands.c
@@ -2,3 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "argparse.h"
+
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
+
+static const char *const usages[] = {
+    "subcommands [options] [cmd] [args]",
+    NULL,
