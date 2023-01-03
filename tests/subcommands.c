c9fdb51e01348e4454df5da6658af880202c6f11
r2kapig <99670839+r2kapig@users.noreply.github.com>
2023-01-03 08:22:38 +0000

    Pontoon: Update German (de) localization of Thimble

--- tests/subcommands.c
+++ tests/subcommands.c
@@ -23,3 +23,14 @@ cmd_foo(int argc, const char **argv)
     printf("argc: %d\n", argc);
     for (int i = 0; i < argc; i++) {
         printf("argv[%d]: %s\n", i, *(argv + i));
+    }
+    int force = 0;
+    int test = 0;
+    const char *path = NULL;
+    struct argparse_option options[] = {
+        OPT_HELP(),
+        OPT_BOOLEAN('f', "force", &force, "force to do", NULL, 0, 0),
+        OPT_BOOLEAN('t', "test", &test, "test only", NULL, 0, 0),
+        OPT_STRING('p', "path", &path, "path to read", NULL, 0, 0),
+        OPT_END(),
+    };
