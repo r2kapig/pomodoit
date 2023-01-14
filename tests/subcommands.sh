344039c2f74c450d73c54c59ba3f5ef03dd2b28d
r2kapig <99670839+r2kapig@users.noreply.github.com>
2023-01-14 14:13:09 +0000

    api updated

--- tests/subcommands.sh
+++ tests/subcommands.sh
@@ -9,4 +9,5 @@ argv[0]: foo
 after argparse_parse:
 argc: 0'
 
-is "$(./subcommands foo -t -p /path/to/file arg1)" 'executing subcommand 
+is "$(./subcommands foo -t -p /path/to/file arg1)" 'executing subcommand foo
+argc: 
